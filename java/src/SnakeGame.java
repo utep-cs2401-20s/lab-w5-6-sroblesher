53c18d6de4472b5351b03e0ff2554b1e7a1151d7
sroblesher1 <sroblesher1@miners.utep.edu>
2020-02-27 19:18:13 -0600

    Fixed error in findTailExhaustive. Finished Comments. Finished LAB!!!

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -35,7 +35,7 @@ public class SnakeGame {
         for (int i = 0; i < game.length; i++) {
             for (int j = 0; j < game[i].length; j++) {
                 if (game[i][j]) {
-                    if (neighbors(i,j) == 1 && i != headPosition[0] && j != headPosition[1]){
+                    if (neighbors(i,j) == 1 && !(i == headPosition[0] && j == headPosition[1])) {
                         tailAndLength[0] = i;
                         tailAndLength[1] = j;
                     }
@@ -47,7 +47,8 @@ public class SnakeGame {
 
         tailAndLength[2] = length;
 
-        if (tailAndLength[0] == -1) {
+        //If Snake is one square long
+        if (tailAndLength[1] == -1) {
             tailAndLength[0] = headPosition[0];
             tailAndLength[1] = headPosition[1];
         }
@@ -76,6 +77,7 @@ public class SnakeGame {
 
         //CHECKING UPPER SQUARE
         if (currentPosition[0]-1 >= 0)
+            //CHECKING FOR OUT OF BOUNDS, AND FOR PREVIOUS
             if (currentPosition[0]-1 != previousPosition[0] || currentPosition[1] != previousPosition[1]) {
                 if (game[currentPosition[0] - 1][currentPosition[1]]) {
                     int[] current = {currentPosition[0]-1, currentPosition[1]};
@@ -86,6 +88,7 @@ public class SnakeGame {
 
         //CHECKING DOWN SQUARE
         if (currentPosition[0]+1 < game.length)
+            //CHECKING FOR OUT OF BOUNDS, AND FOR PREVIOUS
             if (currentPosition[0]+1 != previousPosition[0] || currentPosition[1] != previousPosition[1]) {
                 if (game[currentPosition[0] + 1][currentPosition[1]]) {
                     int[] current = {currentPosition[0]+1, currentPosition[1]};
@@ -96,6 +99,7 @@ public class SnakeGame {
 
         //CHECKING LEFT SQUARE
         if (currentPosition[1]-1 >= 0)
+            //CHECKING FOR OUT OF BOUNDS, AND FOR PREVIOUS
             if (currentPosition[0] != previousPosition[0] || currentPosition[1]-1 != previousPosition[1]) {
                 if (game[currentPosition[0]][currentPosition[1]-1]) {
                     int[] current = {currentPosition[0], currentPosition[1]-1};
@@ -106,6 +110,7 @@ public class SnakeGame {
 
         //CHECKING RIGHT SQUARE
         if (currentPosition[1]+1 < game.length)
+            //CHECKING FOR OUT OF BOUNDS, AND FOR PREVIOUS
             if (currentPosition[0] != previousPosition[0] || currentPosition[1]+1 != previousPosition[1]) {
                 if (game[currentPosition[0]][currentPosition[1]+1]) {
                     int[] current = {currentPosition[0], currentPosition[1]+1};
@@ -133,6 +138,7 @@ public class SnakeGame {
     public int neighbors(int xCoord, int yCoord) {
         int count = 0;
 
+        //Checking for Up, Left, Right and Down respectively
         if (xCoord - 1 >= 0) if (game[xCoord-1][yCoord]) count++;
         if (yCoord - 1 >= 0) if (game[xCoord][yCoord-1]) count++;
         if (yCoord + 1 <= game.length-1) if (game[xCoord][yCoord+1]) count++;
--- java/src/SnakeGameTester.java
+++ java/src/SnakeGameTester.java
@@ -4,27 +4,34 @@ class SnakeGameTester {
 
     /* TESTS FOR FINDING TAIL RECURSIVELY */
 
-    /* First Case: Create a 7x7 boolean board */
+    /* 1st Test Case: Checks when the entire body of the Snake is in
+       the border. Also tests when the head and tail are in the same row.
+       Thanks to this case I noticed and fixed a mistake.               */
+    //STATUS: PASSED
     @org.junit.jupiter.api.Test
     void findTailExhaustive1() {
-        boolean arr[][] = new boolean[7][7];
-        arr[3][3] = true;
-        arr[2][3] = true;
-        arr[1][3] = true;
-        arr[0][3] = true;
-        arr[0][4] = true;
-        arr[0][5] = true;
+        boolean[][] arr = new boolean[3][3];
+        arr[0][0] = true;
+        arr[1][0] = true;
+        arr[2][0] = true;
+        arr[2][1] = true;
+        arr[2][2] = true;
+        arr[1][2] = true;
+        arr[0][2] = true;
 
-        SnakeGame a = new SnakeGame(arr, 3, 3);
+        SnakeGame a = new SnakeGame(arr, 0, 0);
 
-        int expected[] = {0,5,6};
+        int[] expected = {0,2,7};
 
         assertArrayEquals(expected, a.findTailExhaustive());
     }
 
+    /* 2nd Test Case: Testing when the head is in one border of the game/board
+     * and the tail is in the center. */
+    //STATUS: PASSED
     @org.junit.jupiter.api.Test
     void findTailExhaustive2() {
-        boolean arr[][] = new boolean[7][7];
+        boolean[][] arr = new boolean[7][7];
         arr[3][3] = true;
         arr[2][3] = true;
         arr[1][3] = true;
@@ -34,26 +41,32 @@ class SnakeGameTester {
 
         SnakeGame a = new SnakeGame(arr, 0, 5);
 
-        int expected[] = {3,3,6};
+        int[] expected = {3,3,6};
 
         assertArrayEquals(expected, a.findTailExhaustive());
     }
 
+    /* 3rd Test Case: Testing method when Snake is only one cell long,
+    *  therefore head is the same as tail, so the test should return head */
+    //STATUS: PASSED
     @org.junit.jupiter.api.Test
     void findTailExhaustive3() {
-        boolean arr[][] = new boolean[3][3];
+        boolean[][] arr = new boolean[3][3];
         arr[1][1] = true;
 
         SnakeGame a = new SnakeGame(arr, 1,1);
 
-        int expected[] = {1,1,1};
+        int[] expected = {1,1,1};
 
         assertArrayEquals(expected, a.findTailExhaustive());
     }
 
+    /* 4th Test Case: Tests when head is in one corner of the game.
+       Snake changes direction two times, like a "Z" shape */
+    //STATUS: PASSED
     @org.junit.jupiter.api.Test
     void findTailExhaustive4() {
-        boolean arr[][] = new boolean[5][5];
+        boolean[][] arr = new boolean[5][5];
         arr[4][4] = true;
         arr[4][3] = true;
         arr[3][3] = true;
@@ -64,14 +77,17 @@ class SnakeGameTester {
 
         SnakeGame a = new SnakeGame(arr, 4, 4);
 
-        int expected[] = {0,2,7};
+        int[] expected = {0,2,7};
 
         assertArrayEquals(expected, a.findTailExhaustive());
     }
 
+    /* 5th Test case: Checking if method works when Tail is in one corner
+       of the board/game, L shape Snake */
+    //STATUS: PASSED
     @org.junit.jupiter.api.Test
     void findTailExhaustive5() {
-        boolean arr[][] = new boolean[6][6];
+        boolean[][] arr = new boolean[6][6];
         arr[0][0] = true;
         arr[1][0] = true;
         arr[2][0] = true;
@@ -82,33 +98,40 @@ class SnakeGameTester {
 
         SnakeGame a = new SnakeGame(arr, 3, 3);
 
-        int expected[] = {0,0,7};
+        int[] expected = {0,0,7};
 
         assertArrayEquals(expected, a.findTailExhaustive());
     }
 
     /* TESTS FOR FINDING TAIL RECURSIVELY */
 
+    /* 6th Test Case: Testing when the whole body of the Snake is in the border
+       of the game/board. Every part of the snake has out of bounds neighbors  */
+    //STATUS: PASSED
     @org.junit.jupiter.api.Test
     void findTailRecursive1() {
-        boolean arr[][] = new boolean[7][7];
-        arr[3][3] = true;
-        arr[2][3] = true;
-        arr[1][3] = true;
-        arr[0][3] = true;
-        arr[0][4] = true;
-        arr[0][5] = true;
+        boolean[][] arr = new boolean[3][3];
+        arr[0][0] = true;
+        arr[1][0] = true;
+        arr[2][0] = true;
+        arr[2][1] = true;
+        arr[2][2] = true;
+        arr[1][2] = true;
+        arr[0][2] = true;
 
-        SnakeGame a = new SnakeGame(arr, 3, 3);
+        SnakeGame a = new SnakeGame(arr, 0, 0);
 
-        int expected[] = {0,5,6};
+        int[] expected = {0,2,7};
 
         assertArrayEquals(expected, a.findTailRecursive());
     }
 
+    /* 7th Test Case: Testing when the head is on one border of the game/board
+    *  and the tail is in the center. */
+    //STATUS: PASSED
     @org.junit.jupiter.api.Test
     void findTailRecursive2() {
-        boolean arr[][] = new boolean[7][7];
+        boolean[][] arr = new boolean[7][7];
         arr[3][3] = true;
         arr[2][3] = true;
         arr[1][3] = true;
@@ -118,26 +141,32 @@ class SnakeGameTester {
 
         SnakeGame a = new SnakeGame(arr, 0, 5);
 
-        int expected[] = {3,3,6};
+        int[] expected = {3,3,6};
 
         assertArrayEquals(expected, a.findTailRecursive());
     }
 
+    /* 8h Test Case: Testing method when Snake is only one cell long,
+     * therefore head is the same as tail, so the test should return head */
+    //STATUS: PASSED
     @org.junit.jupiter.api.Test
     void findTailRecursive3() {
-        boolean arr[][] = new boolean[3][3];
+        boolean[][] arr = new boolean[3][3];
         arr[1][1] = true;
 
         SnakeGame a = new SnakeGame(arr, 1,1);
 
-        int expected[] = {1,1,1};
+        int[] expected = {1,1,1};
 
         assertArrayEquals(expected, a.findTailRecursive());
     }
 
+    /* 9th Test Case: Tests when head is in one corner of the game.
+       Snake changes direction two times, like a "Z" shape */
+    //STATUS: PASSED
     @org.junit.jupiter.api.Test
     void findTailRecursive4() {
-        boolean arr[][] = new boolean[5][5];
+        boolean[][] arr = new boolean[5][5];
         arr[4][4] = true;
         arr[4][3] = true;
         arr[3][3] = true;
@@ -148,14 +177,17 @@ class SnakeGameTester {
 
         SnakeGame a = new SnakeGame(arr, 4, 4);
 
-        int expected[] = {0,2,7};
+        int[] expected = {0,2,7};
 
         assertArrayEquals(expected, a.findTailRecursive());
     }
 
+    /* 10th Test case: Checking if method works when Tail is in one corner
+       of the board/game, L shape Snake */
+    //STATUS: PASSED
     @org.junit.jupiter.api.Test
     void findTailRecursive5() {
-        boolean arr[][] = new boolean[6][6];
+        boolean[][] arr = new boolean[6][6];
         arr[0][0] = true;
         arr[1][0] = true;
         arr[2][0] = true;
@@ -166,7 +198,7 @@ class SnakeGameTester {
 
         SnakeGame a = new SnakeGame(arr, 3, 3);
 
-        int expected[] = {0,0,7};
+        int[] expected = {0,0,7};
 
         assertArrayEquals(expected, a.findTailRecursive());
     }
